bad0628fff3802b834617ca0e702ee259439c8f6
Kenneth A. Jones II <kenneth@nvnctechnology.com>
2017-02-24 20:23:42 -0600

    added retropie shell scripts

---
+++ autostart.sh
@@ -0,0 +1,2 @@
+# Auto start custom script.  Script will start emulationstation
+python /home/pi/minismartcontroller/pyMiniSmartController/py_msc.py -e &
---
+++ runcommand-onstart.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo "$3" > /home/pi/minismartcontroller/pyMiniSmartController/romdetails.txt
